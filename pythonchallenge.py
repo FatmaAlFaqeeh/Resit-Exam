703aee8db23c744e348c2b3d48e1bf146ebd188c
Fatma Al-Faqeeh <alfaqeehbella@gmail.com>
2023-12-01 19:10:43 +0000

    for loop to iterate through available coins

--- pythonchallenge.py
+++ pythonchallenge.py
@@ -1,4 +1,4 @@
-def calculate_pettychange(cost):
+def calculate_change(cost):
     available_coins = {
         2.0: 2,
         1.0: 6,
@@ -9,3 +9,16 @@ def calculate_pettychange(cost):
         0.02: 4,
         0.01: 6,
     }
+
+    result = {coin: 0 for coin in available_coins}
+
+    remaining_cost = round(cost, 2)
+
+    for coin in sorted(available_coins.keys(), reverse=True):
+        while remaining_cost >= coin and available_coins[coin] > 0:
+            remaining_cost -= coin
+            remaining_cost = round(remaining_cost, 2)
+            available_coins[coin] -= 1
+            result[coin] += 1
+
+
