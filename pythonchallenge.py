0e70b56365d6e70e114b26db3fc02c36a4f17029
Fatma Al-Faqeeh <alfaqeehbella@gmail.com>
2023-12-01 18:55:04 +0000

    calculate change function

---
+++ pythonchallenge.py
@@ -0,0 +1,11 @@
+def calculate_pettychange(cost):
+    available_coins = {
+        2.0: 2,
+        1.0: 6,
+        0.5: 10,
+        0.2: 4,
+        0.1: 6,
+        0.05: 7,
+        0.02: 4,
+        0.01: 6,
+    }
